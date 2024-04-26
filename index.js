0870269f05b1b9e5eafea8e7bc36fdca6f2c90cf
unknown <you@example.com>
2024-04-27 02:14:37 +0500

    created number guessing game

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+console.log(randomNumber);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Guess a number 1 - 10: ",
+    }
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("congratulation!you gussed a right number");
+}
+else {
+    console.log("sorry! you guessed a wrong number");
+}
